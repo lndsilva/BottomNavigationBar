1b8383a15cda5c800a7953e7672f3ae46b9b1dc6
lndsilva <lndsilva@hotmail.com>
2022-08-09 16:00:17 -0300

    implementando fragment no navigationview

--- app/src/main/java/br/com/myskout/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/br/com/myskout/bottomnavigationbar/MainActivity.java
@@ -1,14 +1,62 @@
 package br.com.myskout.bottomnavigationbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    NotificationsFragment notificationsFragment = new NotificationsFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+
+        getSupportFragmentManager().
+                beginTransaction().
+                replace(R.id.frmContainer, homeFragment).commit();
+
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.mNotifications);
+        badgeDrawable.setVisible(true);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mHome:
+                        getSupportFragmentManager().
+                                beginTransaction().
+                                replace(R.id.frmContainer, homeFragment).commit();
+                        return true;
+                    case R.id.mNotifications:
+                        getSupportFragmentManager().
+                                beginTransaction().
+                                replace(R.id.frmContainer, notificationsFragment).commit();
+                        return true;
+                    case R.id.mSettings:
+                        getSupportFragmentManager().
+                                beginTransaction().
+                                replace(R.id.frmContainer, settingsFragment).commit();
+                        return true;
+                }
+
+
+                return false;
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/br/com/myskout/bottomnavigationbar/NotificationsFragment.java
@@ -0,0 +1,25 @@
+package br.com.myskout.bottomnavigationbar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NotificationsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        return view;
+    }
+}
